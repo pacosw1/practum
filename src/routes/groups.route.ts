fe3c456ad2e563a4e4ca4f9cfe7b25af21354b3b
Francisco Sainz <pacoswilliams@gmail.com>
2022-10-09 17:44:36 -0500

    Revert "add auth middleware to modifying routes"

--- src/routes/groups.route.ts
+++ src/routes/groups.route.ts
@@ -3,7 +3,6 @@ import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
 import { CreateGroupDto } from '@/dtos/groups.dto';
 import GroupsController from '@/controllers/groups.controller';
-import authMiddleware from '@middlewares/auth.middleware';
 
 class GroupsRoute implements Routes {
   public path = '/groups';
@@ -17,9 +16,9 @@ class GroupsRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}`, this.controller.getAll);
     this.router.get(`${this.path}/:id(\\d+)`, this.controller.getGivenId);
-    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(CreateGroupDto, 'body'), this.controller.create);
-    this.router.put(`${this.path}/:id(\\d+)`, authMiddleware, validationMiddleware(CreateGroupDto, 'body', true), this.controller.update);
-    this.router.delete(`${this.path}/:id(\\d+)`, authMiddleware, this.controller.delete);
+    this.router.post(`${this.path}`, validationMiddleware(CreateGroupDto, 'body'), this.controller.create);
+    this.router.put(`${this.path}/:id(\\d+)`, validationMiddleware(CreateGroupDto, 'body', true), this.controller.update);
+    this.router.delete(`${this.path}/:id(\\d+)`, this.controller.delete);
   }
 }
 
