7bf88b73c045fbb12f7603a998bfd5b89e4fb3ea
Francisco Sainz <pacoswilliams@gmail.com>
2022-10-22 15:16:52 -0500

    Added Entry Api

---
+++ src/controllers/entries.controller.ts
@@ -0,0 +1,67 @@
+import { NextFunction, Request, Response } from 'express';
+import { Entry } from '@prisma/client';
+import GroupService from '@/services/groups.service';
+import { CreateGroupDto } from '@/dtos/groups.dto';
+import EntryService from '@/services/entry.service';
+import { CreateEntryExitDto } from '@/dtos/process.dto';
+
+class EntriesController {
+  public service = new EntryService();
+
+  public getAll = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const groups: Entry[] = await this.service.getAll();
+
+      res.status(200).json({ data: groups, message: 'findAll' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+//   public getGivenId = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//     try {
+//       const id = Number(req.params.id);
+//       const findArea: Entry = await this.service(id);
+
+//       res.status(200).json({ data: findArea, message: 'findOne' });
+//     } catch (error) {
+//       next(error);
+//     }
+//   };
+
+  public create = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const data: CreateEntryExitDto = req.body;
+      const createData: Entry = await this.service.create(data);
+
+      res.status(201).json({ data: createData, message: 'created' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public update = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const id = Number(req.params.id);
+      const data: CreateEntryExitDto = req.body;
+      const groupData: Entry = await this.service.update(id, data);
+
+      res.status(200).json({ data: groupData, message: 'updated' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public delete = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const id = Number(req.params.id);
+      const deleteData: Entry = await this.service.delete(id);
+
+      res.status(200).json({ data: deleteData, message: 'deleted' });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default EntriesController;
---
+++ src/routes/entries.route.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import validationMiddleware from '@middlewares/validation.middleware';
+import { CreateGroupDto } from '@/dtos/groups.dto';
+import GroupsController from '@/controllers/groups.controller';
+import authMiddleware from '@middlewares/auth.middleware';
+import EntriesController from '@/controllers/entries.controller';
+import { CreateEntryExitDto } from '@/dtos/process.dto';
+
+class EntriesRoute implements Routes {
+  public path = '/entries';
+  public router = Router();
+  public controller = new EntriesController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.controller.getAll);
+    // this.router.get(`${this.path}/:id(\\d+)`, this.controller.getGivenId);
+    this.router.post(`${this.path}`, validationMiddleware(CreateEntryExitDto, 'body'), this.controller.create);
+    this.router.put(`${this.path}/:id(\\d+)`, validationMiddleware(CreateEntryExitDto, 'body', true), this.controller.update);
+    this.router.delete(`${this.path}/:id(\\d+)`, this.controller.delete);
+  }
+}
+
+export default EntriesRoute;
