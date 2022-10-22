679f639f0ed0d11b4951254c3e1eb038b3f52164
Francisco Sainz <pacoswilliams@gmail.com>
2022-10-22 15:18:37 -0500

    Refactor + Add Routes
    added missing routes and refactored route array

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,22 @@ import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 import AreasRoute from './routes/areas.route';
+import EntriesRoute from './routes/entries.route';
 import GroupsRoute from './routes/groups.route';
+import OutputsRoute from './routes/outputs.route';
 import ProcessRoute from './routes/process.route';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute(), new AreasRoute(), new GroupsRoute(), new ProcessRoute()]);
+const app = new App([
+    new IndexRoute(), 
+    new UsersRoute(), 
+    new AuthRoute(), 
+    new AreasRoute(), 
+    new GroupsRoute(), 
+    new ProcessRoute(),
+    new EntriesRoute(),
+    new OutputsRoute()
+]);
 
 app.listen();
