47dacb3d465ad0d09ca9622620b2436b59b8e400
Francisco Sainz <pacoswilliams@gmail.com>
2022-10-22 15:15:55 -0500

    Remove auth cookie temp
    for testing, disable authentication until we can host everything from the same domain

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -17,9 +17,9 @@ class UsersRoute implements Routes {
   private initializeRoutes() {
     this.router.get(`${this.path}`, this.usersController.getUsers);
     this.router.get(`${this.path}/:id(\\d+)`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id(\\d+)`, authMiddleware, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id(\\d+)`, authMiddleware, this.usersController.deleteUser);
+    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put(`${this.path}/:id(\\d+)`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    this.router.delete(`${this.path}/:id(\\d+)`, this.usersController.deleteUser);
   }
 }
 
