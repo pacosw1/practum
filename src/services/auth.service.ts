f53ea480c523e79f6b41bfca13b6320625e1115e
Francisco Sainz <pacoswilliams@gmail.com>
2022-11-12 17:28:13 -0600

    tool fix + delete docs

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -85,7 +85,7 @@ class AuthService {
   public createToken(user: User): TokenData {
     const dataStoredInToken: DataStoredInToken = { id: user.id };
     const secretKey: string = SECRET_KEY;
-    const expiresIn: number = 1000000;
+    const expiresIn: number = 1000 * 60 * 60 * 24 * 30; // 30 days
 
     return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
   }
